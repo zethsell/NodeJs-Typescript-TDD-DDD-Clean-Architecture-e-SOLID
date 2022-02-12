7ef2392540cc44979d80d488e2e2fd87f5afaa2c
zeth_sell <marcio.rodrigues@boxti.com.br>
2022-02-11 21:10:04 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,25 +1,22 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-
+import { badRequest } from '../helpers/http-helpers'
 export class SignUpController {
-  handle (httpRequest: HttpRequest): HttpResponse {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      badRequest(new MissingParamError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      badRequest(new MissingParamError('email'))
     }
 
     return {
       statusCode: 200,
-      body: 'ok'
+      body: {
+        name: 'marcio',
+        email: 'marcio@gmail.com'
+      }
     }
   }
 }
