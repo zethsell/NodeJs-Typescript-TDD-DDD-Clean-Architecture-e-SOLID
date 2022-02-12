fdbfb6c1378976e5ca7dc27e9f821b21e8b07fc0
zeth_sell <marcio.rodrigues@boxti.com.br>
2022-02-11 21:18:09 -0300

    feat: ensure SignUpControoller returns 400 if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,8 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
 export class SignUpController {
-  handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
