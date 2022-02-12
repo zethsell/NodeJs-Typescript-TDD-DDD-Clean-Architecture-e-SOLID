af2fb4cb65eff7d5f07c91882245dd3a6f7d0c5c
zeth_sell <marcio.rodrigues@boxti.com.br>
2022-02-11 21:16:40 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,20 +2,11 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
 export class SignUpController {
-  handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      badRequest(new MissingParamError('email'))
-    }
-
-    return {
-      statusCode: 200,
-      body: {
-        name: 'marcio',
-        email: 'marcio@gmail.com'
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
     }
   }
